f178b412928971c0edeecd1e4342f5a1b4e2fbab
Mandeep Singh <mandeepsingh@usf.edu>
2020-02-04 21:24:35 -0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    class Plane
+    {
+        public int maxspeed;
+        public string color;
+
+        public void fullThrottle();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Plane
+    {
+        string color = "White";
+        int maxSpeed = 20000;
+ 
+        static void Main(string[] args)
+        {
+            Plane myObj = new Plane();
+            Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj.maxSpeed);
+            Console.ReadLine();
+           
+        }
+    }
+}
